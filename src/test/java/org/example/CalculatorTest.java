a234280d1661bd4461a2b38e664caeb06e0c4643
yunhuiGwak <kwakyh17@naver.com>
2023-05-17 16:55:40 +0900

    :recycle: 인터페이스로 구현, 양수조건반영한 test refactoring

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
@@ -41,22 +44,50 @@ public class CalculatorTest {
 //    }
 
     // 각각의 연산 수행에서 사칙연산을 한 번에 수행하는 메소드로 Refactoring
+//    @DisplayName("사칙 연산을 수행한다.")
+//    @ParameterizedTest
+//    @MethodSource("formulaAndResult")
+//    void calculateTest(int operand1, String operator, int operand2, int result) {
+//
+//        int calculateResult = Calculator.calculate(operand1, operator, operand2);   //Calculator에 작업을 위임(객체지향)
+//
+//        assertThat(calculateResult).isEqualTo(result);
+//    }
+//
+//    private static Stream<Arguments> formulaAndResult() {
+//        return Stream.of(
+//            arguments(1,"+",2,3),
+//            arguments(1,"-",2,-1),
+//            arguments(4,"*",2,8),
+//            arguments(4,"/",2,2)
+//        );
+//    }
+//
+//    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.")
+//    @Test
+//    void calculateExceptionTest() {
+//        assertThatCode(() -> Calculator.calculate(10, "/", 0))
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessage("0으로는 나눌 수 없습니다.");
+//    }
+
+    // 3차 Refactoring 양수로만 계산할 수 있다. 조건반영
     @DisplayName("사칙 연산을 수행한다.")
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void calculateTest(int operand1, String operator, int operand2, int result) {
 
-        int calculateResult = Calculator.calculate(operand1, operator, operand2);   //Calculator에 작업을 위임(객체지향)
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));   //Calculator에 작업을 위임(객체지향)
 
         assertThat(calculateResult).isEqualTo(result);
     }
 
     private static Stream<Arguments> formulaAndResult() {
         return Stream.of(
-            arguments(1,"+",2,3),
-            arguments(1,"-",2,-1),
-            arguments(4,"*",2,8),
-            arguments(4,"/",2,2)
+                arguments(1,"+",2,3),
+                arguments(1,"-",2,-1),
+                arguments(4,"*",2,8),
+                arguments(4,"/",2,2)
         );
     }
 }
