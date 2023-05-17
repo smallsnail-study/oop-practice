fb73c93ab45114388f966642a3cabec71210e72f
yunhuiGwak <kwakyh17@naver.com>
2023-05-17 16:51:30 +0900

    :sparkles: 연산자와, 계산을 별도로 분리 후 인터페이스로 생성

---
+++ src/main/java/org/example/calculate/NewArithmeticOperator.java
@@ -0,0 +1,10 @@
+package org.example.calculate;
+
+public interface NewArithmeticOperator {
+    boolean supports(String operator);  //operator를 지원하는지에 대한 메소드
+
+//    int calculate(int operand1, int operand2);
+
+    // 3차 Refactoring 양수로만 계산할 수 있다. 조건반영
+    int calculate(PositiveNumber operand1, PositiveNumber operand2);
+}
