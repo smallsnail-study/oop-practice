481228d16f04f9a8921e64565486d20a316c785b
yunhuiGwak <kwakyh17@naver.com>
2023-05-19 14:59:35 +0900

    :recycle: menuItem에서 가져온 이름과 일치여부 메소드 refactoring - 기존에 test 진행을 위해 하드코딩한 부분 refactoring

--- src/main/java/org/example/order/Menu.java
+++ src/main/java/org/example/order/Menu.java
@@ -10,6 +10,9 @@ public class Menu {
     }
 
     public MenuItem choose(String name) {
-        return new MenuItem("돈까스", 5000);
+        return this.menuItems.stream()
+                .filter(menuItem -> menuItem.matches(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 메뉴 이름입니다."));
     }
 }
