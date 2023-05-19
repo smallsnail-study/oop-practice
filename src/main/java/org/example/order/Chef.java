6586c60e69fda90c724294318a338e4c5c75e6e4
yunhuiGwak <kwakyh17@naver.com>
2023-05-19 14:02:11 +0900

    :recycle: menuItem을 가져와서 makeCook 하도록 refactoring - 테스트 진행을 위해 설정한 하드코딩 변경

--- src/main/java/org/example/order/Chef.java
+++ src/main/java/org/example/order/Chef.java
@@ -3,6 +3,7 @@ package org.example.order;
 // 요리사
 public class Chef {
     public Cook makeCook(MenuItem menuItem) {
-        return new Cook("돈까스", 5000);
+        Cook cook = new Cook(menuItem);
+        return cook;
     }
 }
