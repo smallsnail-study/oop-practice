442bee8acdfd1ead4022e4a831f2de11e4e5fa5b
yunhuiGwak <kwakyh17@naver.com>
2023-05-19 14:52:12 +0900

    :sparkles: 메뉴판 class 생성 - menuItem을 List 타입으로 구성 - 메뉴판 테스트를 위해 메뉴아이템에서 골라오는 메소드 하드코딩

---
+++ src/main/java/org/example/order/Menu.java
@@ -0,0 +1,15 @@
+package org.example.order;
+
+import java.util.List;
+
+public class Menu {
+    private final List<MenuItem> menuItems;
+
+    public <E> Menu(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public MenuItem choose(String name) {
+        return new MenuItem("돈까스", 5000);
+    }
+}
