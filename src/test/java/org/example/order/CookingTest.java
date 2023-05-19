2d8ef2b2b4f7610fb4e7a703ea4b93bd595baa2b
yunhuiGwak <kwakyh17@naver.com>
2023-05-19 14:49:25 +0900

    :bulb: 주석추가

--- src/test/java/org/example/order/CookingTest.java
+++ src/test/java/org/example/order/CookingTest.java
@@ -11,11 +11,13 @@ public class CookingTest {
     void makeCookTest() {
         //요리사 객체
         Chef chef = new Chef();
+        // 요리사에게 전달할 메뉴항목 객체
         MenuItem menuItem = new MenuItem("돈까스", 5000);
 
         // 요리사가 요리를 만들어달라고 요청하고 -> 어떤걸 만들지는 menuItem을 전달
         Cook cook = chef.makeCook(menuItem);
 
+        // 전달된 요리항목과 메뉴항목의 일치여부 확인
         // 객체들끼리 비교할때는 equals(),hashCode()가 필요
         assertThat(cook).isEqualTo(new Cook("돈까스", 5000));
     }
