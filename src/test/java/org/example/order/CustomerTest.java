5bdef0b671b4fe24855d0b0885fc8ab4f2a6e03d
yunhuiGwak <kwakyh17@naver.com>
2023-05-19 15:20:04 +0900

    :white_check_mark: 손님의 주문 test

--- src/test/java/org/example/order/CustomerTest.java
+++ src/test/java/org/example/order/CustomerTest.java
@@ -1,12 +1,17 @@
 package org.example.order;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 /**
  * 음식점에서 음식 주문하는 과정 구현
  * 요구사항
  * 1. 도메인을 구성하는 객체에는 어떤 것들이 있는지 고민
- *    ㄴ 손님, 메뉴판, 돈까스/냉면/만두(O), 요리사, 요리(O)
+ *    ㄴ 손님, 메뉴판(O), 돈까스/냉면/만두(O), 요리사(O), 요리(O)
  * 2. 객체들 간의 관계를 고민
  *    ㄴ 손님 -- 메뉴판
  *    ㄴ 손님 -- 요리사
@@ -22,8 +27,15 @@ import org.junit.jupiter.api.Test;
  */
 public class CustomerTest {
 
+    @DisplayName("메뉴이름에 해당하는 요리를 주문한다.")
     @Test
-    void name() {
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스",5000), new MenuItem("냉면",7000)));
+        Chef chef = new Chef();
 
+        // 주문하면 -> 메뉴항목,요리사의 정보를 전달
+        assertThatCode(()->customer.order("돈까스",menu,chef))
+                .doesNotThrowAnyException();
     }
 }
