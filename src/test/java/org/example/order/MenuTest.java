00288d3f0c57d9ecd76da05220c224ae92f08391
yunhuiGwak <kwakyh17@naver.com>
2023-05-19 15:07:15 +0900

    :white_check_mark: 메뉴판에 없는 메뉴를 선택할 경우 test 추가

--- src/test/java/org/example/order/MenuTest.java
+++ src/test/java/org/example/order/MenuTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class MenuTest {
     @DisplayName("메뉴판에서 메뉴이름에 해당하는 메뉴를 반환한다.")
@@ -18,5 +19,15 @@ public class MenuTest {
 
         // 객체간의 비교를 위해 equals() and hashCode() 필요
         assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
+    }
+
+    @DisplayName("메뉴판에 없는 메뉴를 선택할 시 예외를 반환한다.")
+    @Test
+    void chooseTest2() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스",5000), new MenuItem("냉면",7000)));
+
+        assertThatCode(() -> menu.choose("통닭"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 메뉴 이름입니다.");
     }
 }
