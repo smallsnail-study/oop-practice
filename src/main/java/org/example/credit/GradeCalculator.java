c632d356d80cc344a8037bcd3a3b41fdf23b1cd8
yunhuiGwak <kwakyh17@naver.com>
2023-05-19 13:10:03 +0900

    :recycle: 일급컬렉션을 직접 받아오는 것도 가능하게 refactoring

--- src/main/java/org/example/credit/GradeCalculator.java
+++ src/main/java/org/example/credit/GradeCalculator.java
@@ -15,6 +15,15 @@ public class GradeCalculator {
     public GradeCalculator(List<Course> courses) {
         this.courses = new Courses(courses);
     }
+
+    // 일급 컬렉션으로 직접 받아 오는 것도 가능
+    public GradeCalculator(Courses courses) {
+        this.courses = courses;
+    }
+
+// <핵심포인트> 협력을 설계
+// 이수한 과목을 전달하여 평균학점 계산 요청 --> 학점계산기 -->(학점수 x 교과목평점)의 합계 ---> 과목(코스) 일급 컬렉션
+//                                                  --> 수강신청 총 학점 수 ----> 과목(코스) 일급 컬렉션
     public double calculateGrade() {
 
         //(과목당 학점수 × 교과목 평점)의 합계 일급 컬렉션에 위임
